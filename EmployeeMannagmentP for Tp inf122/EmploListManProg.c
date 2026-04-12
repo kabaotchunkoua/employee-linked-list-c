b462c7eb362397cc98a643a363b1a276d17ddd1e
kabaotchunkoua <kabaotchunkoua@gmail.com>
2026-04-12 06:51:44 +0000

    i just from addin some helper that is in id existe, find by name, find by id, comput rank

--- EmployeeMannagmentP for Tp inf122/EmploListManProg.c
+++ EmployeeMannagmentP for Tp inf122/EmploListManProg.c
@@ -34,8 +34,64 @@ typedef struct Employee {
 
 /* Global list head */
 Employee* head = NULL; 
+
+Employee *create_employee(const char*name, int id, const char* manager, int rank){
+    Employee* newEmpl = (Employee*)malloc(sizeof(Employee)){
+
+        if (!newEmpl){
+            printf("Memory allocation failed\n");
+            exit(1)
+        }
+        strcpy(newEmpl->name, name);
+        newEmpl->id = id;
+        strcpy(newEmpl->management, management);
+        newEmpl->rank = rank;
+        newEmpl->next =NULL;
+        return newEmpl;
+    }
+
+}
+
+int id_exists(int id){
+    Employee * temp = head;
+    while(temp){
+        if (temp->id == id ) return 1;
+        temp = temp->next;
+
+    }
+    return 0;
+}
+
+Employee *find_by_name(const char* name){
+    Employee *tmep= head;
+    while(tmep){
+        if(strcmp(tmep->name, name) == 0) return tmep;
+        tmep = tmep->next;
+    }
+    return NULL;
+}
+Employee *find_by_id(int id){
+    Employee *temp = head;
+    while(temp){
+        if(temp -> id == id) return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+// to the comput the rank based on manager name
+int compute_rank(const char* manager){
+    if(manager == NULL || strlen(manager) == 0) return 1;
+    Employee* mngr = find_by_name(manager);
+    if(mngr) return mngr->rank +1;
+    printf("Warning: Manager '%s' not found. Setting rank = 1. \n", manager);
+    return 1; 
+}
+
+
+
 // The Main method is by loop calls functions via menu selection.
 int main()
 {
-    
+
 }
